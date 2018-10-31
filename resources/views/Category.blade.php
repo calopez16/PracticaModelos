239c034fcf639566c2c7d64db61c30487823f1b0
Carlos Lopez <carlos.armando.lopez.espinoza@uabc.edu.mx>
2018-10-30 19:48:23 -0700

    categoryblade cambio

--- resources/views/Category.blade.php
+++ resources/views/Category.blade.php
@@ -15,7 +15,7 @@
 
     <!-- {{$categorys=App\Category::all()}} -->
 
-    @foreach ({{$categorys=App\Category::all()}} as $category)
+    @foreach (($categorys=App\Category::all()) as $category)
         <p>This is user {{ $category->id }}</p>
     @endforeach
 
