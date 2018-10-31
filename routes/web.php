303b9575911f2c6872203f96de5efd4431f4a5da
Carlos Lopez <carlos.armando.lopez.espinoza@uabc.edu.mx>
2018-10-30 19:10:26 -0700

    cambio de ruta

--- routes/web.php
+++ routes/web.php
@@ -41,4 +41,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/Movements','Movements@index');
-Route::get('/Category','Category@index');
+Route::get('/Category','CategoryController@index');
