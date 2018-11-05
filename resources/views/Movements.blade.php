6e19805bb2e21ae2b338c75addd86d1f0f97ad47
Carlos Lopez <carlos.armando.lopez.espinoza@uabc.edu.mx>
2018-11-04 18:14:23 -0800

    nuevo middleware

--- resources/views/Movements.blade.php
+++ resources/views/Movements.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Movements</div>
                     <h1>Tienes mas de 18!</h1>
                 
                 </div>
