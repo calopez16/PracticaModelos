bb45e1c0fa2046f54099941b4f174b0be0e4bdac
Carlos Lopez <carlos.armando.lopez.espinoza@uabc.edu.mx>
2018-11-04 18:12:03 -0800

    nuevo middleware

--- resources/views/Movements.blade.php
+++ resources/views/Movements.blade.php
@@ -1,21 +1,8 @@
 @extends('layouts.app')
 
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-
-</header>
-<body>
+@section('content')
 <h1>Pagina de Movements</h1>
 <p>Tienes mas de 18 a egg!</p>
-</body>
 
 
-
-</html>
+@endsection
