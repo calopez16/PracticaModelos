0cca24fb437b509c3637365a26b500049d9ae46a
Carlos Lopez <carlos.armando.lopez.espinoza@uabc.edu.mx>
2018-11-08 00:07:46 -0800

    vista

--- resources/views/Category.blade.php
+++ resources/views/Category.blade.php
@@ -2,14 +2,26 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                 <!-- {{$categorys=App\Category::all()}} -->
+
+                    @foreach (($categorys=App\Category::all()) as $category)
+                        <p>This is user {{ $category->id }}</p>
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
+    </div>
 <h1>Categorys</h1>
 
 
-    <!-- {{$categorys=App\Category::all()}} -->
-
-    @foreach (($categorys=App\Category::all()) as $category)
-        <p>This is user {{ $category->id }}</p>
-    @endforeach
 
     
 @endsection
